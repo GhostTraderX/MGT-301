41d06687aed6fd9b30c0c2b13d96dfe788593353
GhostTraderX <126263543+GhostTraderX@users.noreply.github.com>
2024-11-18 12:17:21 +0100

    Finished question 2

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
+import math
 import pandas as pd
 
-spy_data = pd.read_csv('SPY.csv')
-ewl_data = pd.read_csv('EWL.csv')
-ief_data = pd.read_csv('IEF.csv')
 
+risk_free_rate = 0.015
+
+# Import data
+spy_data = pd.read_csv('SPY.csv')['adjClose']
+ewl_data = pd.read_csv('EWL.csv')['adjClose']
+ief_data = pd.read_csv('IEF.csv')['adjClose']
+df = pd.DataFrame({'SPY': spy_data, 'EWL': ewl_data, 'IEF':ief_data})
+
+# Calculate annualized expected returns (aer)
+def get_annualized_expected_returns(data):
+    pct_change = data.pct_change().dropna()
+    mean = pct_change.mean()
+    return mean * 52
+
+spy_aer = get_annualized_expected_returns(spy_data)
+ewl_aer = get_annualized_expected_returns(ewl_data)
+ief_aer = get_annualized_expected_returns(ief_data)
+
+# Calculate annualized volatility
+def get_annualized_volatility(data):
+    pct_change = data.pct_change().dropna()
+    var = pct_change.var()
+    return math.sqrt(var * 52)
+
+spy_vol = get_annualized_volatility(spy_data)
+ewl_vol = get_annualized_volatility(ewl_data)
+ief_vol = get_annualized_volatility(ief_data)
+
+# calculate pairwise correlation
+spy_ewl_corr = df.corr().loc['SPY', 'EWL']
+spy_ief_corr = df.corr().loc['SPY', 'IEF']
+ewl_ief_corr = df.corr().loc['EWL', 'IEF']
