0601053a00d2b438e34b4aed4e36e1827efb14e4
AKR-115 <126262976+AKR-115@users.noreply.github.com>
2024-12-03 15:40:27 +0100

    Question 1 homework 3

---
+++ Homework 3.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# ======== Question 1 =======
+S0 = 1              # Initial risky price
+r = 0.03            # Riskfree rate
+N = 100             # Number of periods
+delta = 1 / N       # Lenght of periods
+U = np.exp(0.02)    # Up factor
+D = 1 / U           # Down factor
+
+def h(a):
+    return 1
+
+def payoff(k, h, S0, r, N, delta, U, D, q_N):
+    print(k)
+    if k > (N - 1):
+        return h(1)
+    else:
+        return q_N * (payoff(k+1, h, S0, r, N, delta, U, D, q_N) / (np.exp(r * delta))) + (1 - q_N) * payoff(k, h, S0, r, N, delta, U, D, q_N) / (np.exp(r * delta))
+
+print(payoff(0, h, S0, r, N, delta, U, D, 0.5))
