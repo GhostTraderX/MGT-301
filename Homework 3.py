963ac9174dcfb3e3b8372bda396016da9bd1985d
AKR-115 <126262976+AKR-115@users.noreply.github.com>
2024-12-12 20:58:11 +0100

    Question 1, update

--- Homework 3.py
+++ Homework 3.py
@@ -20,8 +20,8 @@ def binomial_pricing(S0, k, r, N, delta, U, D, h):
     coeff = 1 / np.exp(r * delta)
 
     # We start the algorithm at the end, so we have to create the array to receive the whole tree
-    stock_prices = np.array([S0 * (U**j) * (D**(N - j)) for j in range(N + 1)])
-    P_t = np.array([h(price) for price in stock_prices]) # This is the payoff function
+    S_n = [S0 * (U**j) * (D**(N - j)) for j in range(N + 1)]
+    P_t = [h(price) for price in S_n] # This is the payoff function
 
     # Now we do the Backward induction
     for n in range(N, 0, -1): # Go from N to 0, using a step of -1, meaning we go backwards
@@ -33,10 +33,10 @@ def binomial_pricing(S0, k, r, N, delta, U, D, h):
     a0 = (U * P_t[0] - D * P_t[1]) / (np.exp(r * delta) * (U - D))
     #a0 = P_t[0] - b0 * S0  # Derived from C0 = a0 + b0 * S0
 
-    return P_t[0], (a0, b0)
+    return P_t[0], a0, b0
 
 # Code test
-C0, (a0, b0) = binomial_pricing(S0, k, r, N, delta, U, D, h)
+C0, a0, b0 = binomial_pricing(S0, k, r, N, delta, U, D, h)
 
 print("Initial price of derivative:", C0)
-print("Initial replicating portfolio:", (a0, b0))
+print("Initial replicating portfolio:", a0, b0)
