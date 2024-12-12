f271c443138a7a6f82251cc1d936c209a794ad1f
AKR-115 <126262976+AKR-115@users.noreply.github.com>
2024-12-12 09:14:27 +0100

    Question 1, update

--- Homework 3.py
+++ Homework 3.py
@@ -19,21 +19,21 @@ def binomial_pricing(S0, k, r, N, delta, U, D, h):
     q = (np.exp(r * delta) - D) / (U - D)
     coeff = 1 / np.exp(r * delta)
 
-    # We start the algorithm at the end, so we have to create the array to acomodate every element
+    # We start the algorithm at the end, so we have to create the array to receive the whole tree
     stock_prices = np.array([S0 * (U**j) * (D**(N - j)) for j in range(N + 1)])
-    option_values = np.array([h(price) for price in stock_prices])
+    P_t = np.array([h(price) for price in stock_prices]) # This is the payoff function
 
     # Now we do the Backward induction
-    for n in range(N, 0, -1): # here we want to go from N to 0, using a step of -1, meaning we go backwards
-        for j in range(n):
-            option_values[j] = coeff * (q * option_values[j + 1] + (1 - q) * option_values[j])
+    for n in range(N, 0, -1): # Go from N to 0, using a step of -1, meaning we go backwards
+        for j in range(n):    # Passing by every branch of the tree
+            P_t[j] = coeff * (q * P_t[j + 1] + (1 - q) * P_t[j])
 
     # We can now compute the replicating portfolio
-    b0 = (option_values[1] - option_values[0]) / (S0 * (U - D))  # option_values[1] means stock goes up and [0] stock goes down
-    a0 = (U * option_values[0] - D * option_values[1]) / (np.exp(r * delta) * (U - D))
-    #a0 = option_values[0] - b0 * S0  # Derived from C0 = a0 + b0 * S0
+    b0 = (P_t[1] - P_t[0]) / (S0 * (U - D))  # option_values[1] means stock goes up and [0] stock goes down
+    a0 = (U * P_t[0] - D * P_t[1]) / (np.exp(r * delta) * (U - D))
+    #a0 = P_t[0] - b0 * S0  # Derived from C0 = a0 + b0 * S0
 
-    return option_values[0], (a0, b0)
+    return P_t[0], (a0, b0)
 
 # Code test
 C0, (a0, b0) = binomial_pricing(S0, k, r, N, delta, U, D, h)
