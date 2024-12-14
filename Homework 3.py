829d1089d90a046db4e35fc2af686d08ac541e2b
AKR-115 <126262976+AKR-115@users.noreply.github.com>
2024-12-14 18:06:43 +0100

    Question 1 done + Question 3 option prices as a function of N (we are forced to start at N=2, don't know if that's a big problem or not)

--- Homework 3.py
+++ Homework 3.py
@@ -7,14 +7,14 @@ np.set_printoptions(precision=3)
 S0 = 1              # Initial risky price
 k = 1               # Strike price
 r = 0.03            # Riskfree rate
-N = 100             # Number of periods
+N = 100            # Number of periods
 delta = 1 / N       # Lenght of periods
 U = np.exp(0.02)    # Up factor
 D = 1 / U           # Down factor
 
 # We are using a European call option, which means:
-def h(S, K = 1):
-    return max(S - K, 0)
+def h(S):
+    return max(S - k, 0)
 
 def binomial_pricing(S0, k, r, N, delta, U, D, h):
 
@@ -28,18 +28,13 @@ def binomial_pricing(S0, k, r, N, delta, U, D, h):
 
     # Now we do the Backward induction
     for n in range(N-1, -1, -1): # Go from N to 0, using a step of -1, meaning we go backwards
-        #print(n)
         for j in range(n+1): # Passing by every branch of the tree
-            #print(j)
             P_N[j] = coeff * (q * P_N[j + 1] + (1 - q) * P_N[j])
-        if n == 1:
-            b0 = (P_N[1] - P_N[0]) / (S_n[1] - S_n[0])  # option_values[1] means stock goes up and [0] stock goes down
+        if n == 1:      # The given formula makes us use the penultimate branch instead of the last branch
             a0 = (U * P_N[0] - D * P_N[1]) / (np.exp(r * delta) * (U - D))
-        #print(P_N)
 
-    # We can now compute the replicating portfolio
 
-    #a0 = P_t[0] - b0 * S0  # Derived from C0 = a0 + b0 * S0
+    b0 =  (P_N[0] - a0) / S0  # Derived from C0 = a0 + b0 * S0
     print(P_N[0])
     print(P_N[1])
     return P_N[0], a0, b0
@@ -49,8 +44,8 @@ C0, a0, b0 = binomial_pricing(S0, k, r, N, delta, U, D, h)
 
 print("Initial price of derivative:", C0)
 print("Initial replicating portfolio:", a0, b0)
-"""
-#========== Question 2 ==========
+
+#========== Question 3 ==========
 # Initial values
 S0 = 30
 r = 0.05
@@ -64,7 +59,7 @@ b_val = []
 
 # Loop to get every result + verify if arbitrage free
 # Our goal is to change N and verify if it works
-for N in range(1, 101):
+for N in range(2, 101):
     delta = T / N
     U = np.exp(0.2 * np.sqrt(delta))
     D = 1 / U
@@ -79,13 +74,11 @@ for N in range(1, 101):
         print("Model with N= ", N, " not arbitrage-free!")
 
 # Plot prices
-N_values = range(1, 101)
+N_values = range(2, 101)
 
 plt.figure()
 plt.plot(N_values, prices)
 plt.title("Option prices as function of N")
 plt.xlabel("Option price")
 plt.ylabel("N")
-#plt.show()
-
-"""
+plt.show()
